26a132af9a6d265e693a6bb03ee9401b8db132c4
whitelistthingloliamsosecretnoonecanseemeimsocoollolreallyareusurelolhowlongcanimakethis <147185847+whitelistbotthinglol@users.noreply.github.com>
2024-06-05 12:09:19 +0300

    Update sha.lua

--- sha.lua
+++ sha.lua
@@ -1,4 +1,3 @@
---This watermark is used to delete the file if its cached, remove it to make the file persist after commits.
 --[=[------------------------------------------------------------------------------------------------------------------------
 -- HashLib by Egor Skriptunoff, boatbomber, and howmanysmall
 
