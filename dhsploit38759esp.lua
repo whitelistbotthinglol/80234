7e42b5be24ed1cbc8ed89a82b96392f97d665a96
whitelistthingloliamsosecretnoonecanseemeimsocoollolreallyareusurelolhowlongcanimakethis <147185847+whitelistbotthinglol@users.noreply.github.com>
2024-06-08 11:04:15 +0300

    Update dhsploit38759esp.lua

--- dhsploit38759esp.lua
+++ dhsploit38759esp.lua
@@ -1,5 +1,5 @@
 getgenv().filluseteamcolor = true --Toggle fill color using player team color on/off
-getgenv().outlineuseteamcolor = false --Toggle outline color using player team color on/off
+getgenv().outlineuseteamcolor = true --Toggle outline color using player team color on/off
 getgenv().fillcolor = Color3.new(0, 0, 0) --Change fill color, no need to edit if using team color
 getgenv().outlinecolor = Color3.new(1, 1, 1) --Change outline color, no need to edit if using team color
 getgenv().filltrans = 1 --Change fill transparency
